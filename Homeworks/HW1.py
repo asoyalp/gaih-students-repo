e3c580cc8fc158ed7752a4bd6216bbb396c30206
Altay Soyalp <59841339+asoyalp@users.noreply.github.com>
2021-03-14 23:51:58 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -11,7 +11,7 @@ list1 = [1,3,5,7,9]
 list2 = [2,4,6,8,10]
 list1.extend(list2)
 list1.sort()
-doubles = [i*2 for i in list1]
-
-for i in doubles:
-  print(type(doubles))
+Integers = [int(i)*2 for i in list1]
+print(Integers)
+for i in Integers:
+  print(type(i))
